ac2ac99f554ceee350163dd94892433edfbd9271
Reham <73202832+rehamessa@users.noreply.github.com>
2022-12-15 13:26:41 +0200

    Add files via upload

--- Procedure/procedure_lec.sql
+++ Procedure/procedure_lec.sql
@@ -88,3 +88,40 @@ where name='UPDATE_EMP_SAL_PORC';
 grant execute on HR.UPDATE_EMP_SAL_PORC to wallet with admin option;
 
 ---------------
+--optional parameter
+---parameter modes :in ,out,in out
+create or replace procedure update_emp_sal_porc_v2(v_salary_after out number,v_emp_id in number,v_bonous_perc number :=10)
+is
+
+
+v_salary number(8,2);
+--v_salary_after number(10,2);
+begin
+select salary into v_salary
+from employees
+where employee_id=v_emp_id;
+
+v_salary_after :=v_salary+v_salary*v_bonous_perc/100;
+update employees set salary=v_salary_after
+where employee_id=v_emp_id;
+end;
+show errors
+---calling procedure2
+declare
+v_salary number(10,2);
+begin
+ update_emp_sal_porc_v2(v_salary,101,15);
+ dbms_output.put_line('salary'||v_salary);
+
+ end;
+ show errors
+ 
+ ---calling subprogramms using named parameters not by position 
+declare
+v_salary number(10,2);
+begin
+
+  update_emp_sal_porc_v2(v_salary,v_bonous_perc=>15,v_emp_id=>101);
+  dbms_output.put_line('salary'||v_salary);
+ end;
+ show errors
